a9accce13eea241d15f59eef99055c56133e82a8
Ghost <mkp0203@live.com>
2018-10-20 13:35:06 -0400

    added stuff

--- public/js/userForm.js
+++ public/js/userForm.js
@@ -1,39 +1,36 @@
 $(document).ready(function () {
 
-    $("#goal").hide();
+    $("#goal").show();
 
-    var counter = 0;
+    var counter = 1;
 
     $("#addRow").on("click", function () {
-        // for (var i = 0; i < counter.length; i++) {
             var newRow = $("<tr>");
             newRow.addClass("row");
             newRow.attr("id", "row" + counter);
-
+            console.log(newRow);
             var cols = "";
-            cols += '<td class="col-sm-3"><input type="text" class="form-control" name="food' + counter + '"/></td>';
-            cols += '<td class="col-sm-2"><input type="text" class="form-control" name="protein' + counter + '"/></td>';
-            cols += '<td class="col-sm-2"><input type="text" class="form-control" name="fat' + counter + '"/></td>';
-            cols += '<td class="col-sm-2"><input type="text" class="form-control" name="carbs' + counter + '"/></td>';
-            cols += '<td class="col-sm-2"><input type="text" class="form-control" name="fiber' + counter + '"/></td>';
+            cols += '<td class="col-sm-3"><input type="text" class="form-control" id="foodName' + counter + '"/></td>';
+            cols += '<td class="col-sm-2"><input type="text" class="form-control" id="calories' + counter + '"/></td>';
+            cols += '<td class="col-sm-2"><input type="text" class="form-control" id="protein' + counter + '"/></td>';
+            cols += '<td class="col-sm-2"><input type="text" class="form-control" id="fat' + counter + '"/></td>';
+            cols += '<td class="col-sm-2"><input type="text" class="form-control" id="carbs' + counter + '"/></td>';
             cols += '<td class="col-sm-1"><span id="clickFA"><i class="ibtnDel fas fa-trash-alt fa-2x"></span></td>';
             newRow.append(cols);
 
             $("#inputTable").append(newRow);
             counter++;
-        // }
-    });
+        });
 
     $("table.order-list").on("click", ".ibtnDel", function (event) {
         $(this).closest("tr").remove();
-        counter -= 1
+        counter -= 1;
     });
 
     var count = 1;
 
     $("#submit").on("click", function (event) {
         event.preventDefault();
-        $("#goal").show();
         $(".form-control").empty();
 
         var $header = $("<h6>Meal " + count++ + "</h6>");
